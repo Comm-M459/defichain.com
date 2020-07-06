328d135357b2d74ce52df5ad1ab6ad4852e8ceb4
Aen <hello@aentan.com>
2020-07-06 13:09:48 +0800

    Publishing to gh-pages (deploy.sh)

--- js/bundle.c1b995f76880a5754565e668338584c85ddd361548f63b4b9a3844183906b398.js
+++ js/bundle.c1b995f76880a5754565e668338584c85ddd361548f63b4b9a3844183906b398.js
@@ -150,6 +150,15 @@ $(function () {
     });
   }
 
+  if ($(".featured-video").length) {
+
+    $('.video-link').each(function () {
+      $(this).click(function () {
+        player.cueVideoById($(this).data('video'));
+      });
+    });
+  }
+
   $('body').scrollspy({
     target: '#TableOfContents',
     offset: 154
